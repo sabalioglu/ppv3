f489e5b2263f888822d90d155ff60fabf8fa332b
sabalioglu <103506816+sabalioglu@users.noreply.github.com>
2025-06-21 14:51:49 +0300

    Updated login.tsx

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginScreen from '@/screens/LoginScreen';
+import LoginScreen from '../../screens/LoginScreen';
 
 export default function LoginRoute() {
   return <LoginScreen />;
