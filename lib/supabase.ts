b8178a8b53ae2ada3fefe774a27ae5a96a2eca6e
sabalioglu <103506816+sabalioglu@users.noreply.github.com>
2025-06-23 15:53:42 +0300

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
@@ -19,9 +20,11 @@ console.log('✅ Supabase Config:', {
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
+    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: true,
+    flowType: 'pkce',
   },
 });
 
