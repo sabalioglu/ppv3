4180d4278c634426451c282e3cd2da1fb536b6a4
Sedanur Akçil <sdnr.akcil@gmail.com>
2025-11-10 02:51:16 +0300

    created auth layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,8 +1,19 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
 import { useTheme } from '@/contexts/ThemeContext';
+import { useAuth } from '@/contexts/AuthContext';
+import LoadingCard from '@/components/UI/LoadingCard';
 
 export default function AuthLayout() {
   const { colors } = useTheme();
+  const { isReady, session, isProfileComplete } = useAuth();
+
+  if (!isReady) {
+    return <LoadingCard />;
+  }
+
+  if (session && session.user && isProfileComplete) {
+    return <Redirect href="/" />;
+  }
 
   return (
     <Stack
@@ -14,44 +25,43 @@ export default function AuthLayout() {
         animation: 'slide_from_right',
       }}
     >
-      <Stack.Screen
-        name="signup"
-        options={{
-          title: 'Sign Up',
-        }}
-      />
-      <Stack.Screen
-        name="login"
-        options={{
-          title: 'Login',
-        }}
-      />
-      <Stack.Screen
-        name="onboarding"
-        options={{
-          title: 'Get Started',
-          gestureEnabled: false,
-        }}
-      />
-      <Stack.Screen
-        name="callback"
-        options={{
-          title: 'Authenticating...',
-          gestureEnabled: false,
-        }}
-      />
-      <Stack.Screen
-        name="reset-password"
-        options={{
-          title: 'Reset Password',
-        }}
-      />
-      <Stack.Screen
-        name="reset-confirm-password"
-        options={{
-          title: 'Reset Confirm Password',
-        }}
-      />
+      <Stack.Protected guard={!session?.user}>
+        <Stack.Screen
+          name="signup"
+          options={{
+            title: 'Sign Up',
+          }}
+        />
+        <Stack.Screen
+          name="login"
+          options={{
+            title: 'Login',
+          }}
+        />
+        <Stack.Screen
+          name="reset-password"
+          options={{
+            title: 'Reset Password',
+          }}
+        />
+        <Stack.Screen
+          name="reset-confirm-password"
+          options={{
+            title: 'Reset Confirm Password',
+          }}
+        />
+      </Stack.Protected>
+      <Stack.Protected
+        guard={!!session?.user && !!isReady && !isProfileComplete}
+      >
+        <Stack.Screen
+          name="onboarding"
+          options={{
+            title: 'Get Started',
+            gestureEnabled: false,
+          }}
+        />
+      </Stack.Protected>
       <Stack.Screen
         name="email-confirmed"
         options={{
