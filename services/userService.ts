a9a1ee7c88d8b0df8d75ca1f512034d17f91d016
Sedanur Akçil <sdnr.akcil@gmail.com>
2026-01-12 22:22:04 +0300

    deleted getTodaysIntake logic

--- services/userService.ts
+++ services/userService.ts
@@ -15,33 +15,3 @@ export const getUserProfile = async (userId: string) => {
     throw new Error('Error fetching user profile:' + err.message);
   }
 };
-
-export const getTodaysIntake = async (userId: string) => {
-  try {
-    const today = new Date().toISOString().split('T')[0];
-
-    const { data, error } = await supabase
-      .from('nutrition_logs')
-      .select('*')
-      .eq('user_id', userId)
-      .eq('date', today);
-
-    if (error) throw error;
-
-    if (!data?.length) {
-      return { calories: 0, protein: 0, carbs: 0, fat: 0 };
-    }
-
-    return data.reduce(
-      (acc, log) => ({
-        calories: acc.calories + (log.calories || 0),
-        protein: acc.protein + (log.protein || 0),
-        carbs: acc.carbs + (log.carbs || 0),
-        fat: acc.fat + (log.fat || 0),
-      }),
-      { calories: 0, protein: 0, carbs: 0, fat: 0 }
-    );
-  } catch (err: Error | any) {
-    throw new Error("Error fetching today's intake: " + err.message);
-  }
-};
